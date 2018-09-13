309274e574969714848047cd8e5b14b5ad88d335
Jeffrey <jeffrey_wii@hotmail.com>
2018-09-13 09:31:02 +0200

    Developers design

--- resources/views/developer/index.blade.php
+++ resources/views/developer/index.blade.php
@@ -1,32 +1,31 @@
 @extends('layouts.master')
 
 @section('seo')
-    @component("components.seo", ["title" => __('breadcrumbs.publishers'), "url" => url("publishers"), "description" => 'A great number of contributors, exhibitors and publishers are represented at Gamescom. For as far is there each year an increasement in their numbers. '] )
+    @component("components.seo", ["title" => __('breadcrumbs.developers'), "url" => url("developers"), "description" => 'A great number of contributors, exhibitors and developers are represented at Gamescom. For as far is there each year an increasement in their numbers. '] )
     @endcomponent
 @endsection
 
 @section('content')
 
     <section id="featured_image_section">
-        <div class="featured_image pinkpurple" style="background-image:url('https://www.gamescomevent.com/img/gamescom_17_010_010.jpg')"></div>
-    </section>
+        <div class="featured_image pinkpurple" style="background-image:url('https://www.gamescomevent.com/img/gamescom_17_010_010.jpg')"></div></section>
 
-    <div class="container publisher-show">
+    <div class="container developer-show">
         <div class="row">
             <div class="col-md-12">
 
                 {{-- Title --}}
-                <h1>{{__('breadcrumbs.publishers')}}</h1>
+                <h1>{{__('breadcrumbs.developers')}}</h1>
 
                 {{-- Breadcrumbs --}}
-                @component('components.breadcrumbs', ['breadcrumbs' => ['publishers' => __('breadcrumbs.publishers')]])
+                @component('components.breadcrumbs', ['breadcrumbs' => ['developers' => __('breadcrumbs.developers')]])
                 @endcomponent
 
-                <p><strong>A great number of contributors, exhibitors and publishers are represented at Gamescom. For as far is there each year an increasement in their numbers.</strong></p>
+                <p><strong>A great number of contributors, exhibitors and developers are represented at Gamescom. For as far is there each year an increasement in their numbers.</strong></p>
                 <br />
 
-                @foreach($publishers as $publisher)
-                    <a href="{{url('publishers/' . $publisher->slug)}}">{{$publisher->title}}</a> <br />
+                @foreach($developers as $developer)
+                    <a href="{{url('developers/' . $developer->slug)}}">{{$developer->title}}</a> <br />
                 @endforeach
 
             </div>
